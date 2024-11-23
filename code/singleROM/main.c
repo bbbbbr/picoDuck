136c7c32909e1956c80df0975caa3ba18f1bec6c
bbbbbr <bbbbbr@users.noreply.github.com>
2024-11-23 15:29:38 -0800

    Up to 512K, not very stable

--- code/singleROM/main.c
+++ code/singleROM/main.c
@@ -42,15 +42,20 @@
 #define RSTMS 100
 
 // Bit masks.
-#define ADDRMASK 0b00000000000000001111111111111111
-#define DATAMASK 0b00000100011111110000000000000000
+#define ADDRMASK    0b00000000000000001111111111111111
+#define DATAMASK    0b00000100011111110000000000000000
+//                                    FEDCBA9876543210
+//                    FEDCBA9876543210
 #define LOWDATAMASK 0b00000000011111110000000000000000
-#define NWRMASK  0b00001000000000000000000000000000
-#define RSTMASK  0b00010000000000000000000000000000
-#define A15MASK  0b00000000000000001000000000000000
+#define NWRMASK     0b00001000000000000000000000000000
+#define RSTMASK     0b00010000000000000000000000000000
+#define A15MASK     0b00000000000000001000000000000000
 
+//                     76543210
 #define DATAMASKLOW  0b01111111
 #define DATAMASKHIGH 0b10000000
+#define BANKMASK     0b00011111
+// #define BANKMASK     0b00000111
 
 // Bank switch
 // 1: Dynamic 32 kB banks (e.g., Puppet Knight)
@@ -71,9 +76,13 @@ void initGPIO() {
   gpio_set_dir( RST, GPIO_OUT );
 }
 
+#define ROM_BANK(N) (rom + ((N-1) * 0x4000u)) // The reason for N-1 is because rombank will be accessed with a base of (0x4000) + the bank relative address
+
+// pico pi 2040: 264kB of SRAM, 2MB of flash storage
+
 void __not_in_flash_func( handleROM() ) {
-  // Initial bank.
-  uint8_t* rombank = rom;
+  // Initial bank, point it to BANK 1
+  const uint8_t * rombank = ROM_BANK(1); // rom;
   
   // Start endless loop.
   while( 1 ) {
@@ -133,39 +142,54 @@ void __not_in_flash_func( handleROM() ) {
           // Bank switch type 2
           BANKSWITCH = 2;
           
-          writeData = ( data & LOWDATAMASK ) & ( 0b111 << DATAOFFSETLOW );
-          
+          writeData = ( data & LOWDATAMASK ) & ( BANKMASK << DATAOFFSETLOW );
+
           // Use a case structure instead of a dynamic multiplcation
           // to hopefully speed up things.
           switch ( writeData ) {
             case 0:
-            case ( 1 << DATAOFFSETLOW ):
-              rombank = rom + 0; 
-              break;
-              
-            case ( 2 << DATAOFFSETLOW ):
-              rombank = rom + 1 * 16384;
-              break;
-              
-            case ( 3 << DATAOFFSETLOW ):
-              rombank = rom + 2 * 16384;
-              break;
-              
-            case ( 4 << DATAOFFSETLOW ):
-              rombank = rom + 3 * 16384;
-              break;
-              
-            case ( 5 << DATAOFFSETLOW ):
-              rombank = rom + 4 * 16384;
-              break;
-              
-            case ( 6 << DATAOFFSETLOW ):
-              rombank = rom + 5 * 16384;
-              break;
-              
-            case ( 7 << DATAOFFSETLOW ):
-              rombank = rom + 6 * 16384;
-              break;
+            case ( 1  << DATAOFFSETLOW ): rombank = ROM_BANK(1); break;
+            // End 32K
+            // 
+            case ( 2  << DATAOFFSETLOW ): rombank = ROM_BANK(2); break;
+            case ( 3  << DATAOFFSETLOW ): rombank = ROM_BANK(3); break;
+            // End 64K
+            
+            case ( 4  << DATAOFFSETLOW ): rombank = ROM_BANK(4); break;
+            case ( 5  << DATAOFFSETLOW ): rombank = ROM_BANK(5); break;
+            case ( 6  << DATAOFFSETLOW ): rombank = ROM_BANK(6); break;
+            case ( 7  << DATAOFFSETLOW ): rombank = ROM_BANK(7); break;
+            // End 128K
+            
+            case ( 8  << DATAOFFSETLOW ): rombank = ROM_BANK(8); break;
+            case ( 9  << DATAOFFSETLOW ): rombank = ROM_BANK(9); break;
+            case ( 10 << DATAOFFSETLOW ): rombank = ROM_BANK(10); break;
+            case ( 11 << DATAOFFSETLOW ): rombank = ROM_BANK(11); break;
+            case ( 12 << DATAOFFSETLOW ): rombank = ROM_BANK(12); break;
+            case ( 13 << DATAOFFSETLOW ): rombank = ROM_BANK(13); break;
+            case ( 14 << DATAOFFSETLOW ): rombank = ROM_BANK(14); break;
+            case ( 15 << DATAOFFSETLOW ): rombank = ROM_BANK(15); break;            
+            // End 256K (seems ok)
+
+            case ( 16 << DATAOFFSETLOW ): rombank = ROM_BANK(16); break;
+            case ( 17 << DATAOFFSETLOW ): rombank = ROM_BANK(17); break;
+            case ( 18 << DATAOFFSETLOW ): rombank = ROM_BANK(18); break;
+            case ( 19 << DATAOFFSETLOW ): rombank = ROM_BANK(19); break;
+            case ( 20 << DATAOFFSETLOW ): rombank = ROM_BANK(20); break;
+            case ( 21 << DATAOFFSETLOW ): rombank = ROM_BANK(21); break;
+            case ( 22 << DATAOFFSETLOW ): rombank = ROM_BANK(22); break;
+            case ( 23 << DATAOFFSETLOW ): rombank = ROM_BANK(23); break;
+            
+            case ( 24 << DATAOFFSETLOW ): rombank = ROM_BANK(24); break;
+            case ( 25 << DATAOFFSETLOW ): rombank = ROM_BANK(25); break;
+            case ( 26 << DATAOFFSETLOW ): rombank = ROM_BANK(26); break;
+            case ( 27 << DATAOFFSETLOW ): rombank = ROM_BANK(27); break;
+            case ( 28 << DATAOFFSETLOW ): rombank = ROM_BANK(28); break;
+            case ( 29 << DATAOFFSETLOW ): rombank = ROM_BANK(29); break;
+            case ( 30 << DATAOFFSETLOW ): rombank = ROM_BANK(30); break;
+            case ( 31 << DATAOFFSETLOW ): rombank = ROM_BANK(31); break;
+            // End 512K (seems ok)
+
 
             // Something went wrong.
             default:
