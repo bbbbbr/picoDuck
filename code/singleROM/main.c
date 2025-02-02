35d54ddd9059868aa042267264c73556c92da62f
bbbbbr <bbbbbr@users.noreply.github.com>
2025-02-02 03:08:22 -0800

    Add MD0 banked 32K ROM support

--- code/singleROM/main.c
+++ code/singleROM/main.c
@@ -57,15 +57,17 @@
 #define A15_TO_13_MASK  0b00000000000000001110000000000000
 #define SRAM_ADDR_MATCH 0b00000000000000001010000000000000  // If A15=HI then: Cart SRAM addrs uniquely have A14=LO, A13=HI
 #define SRAM_RANGE_MASK 0b00000000000000000001111111111111  // 0xBFFF - 0xC000 = 0x1FFF total cart SRAM range
-bank switch reg addr                      0001000000000000
+// bank switch reg addr                   0001000000000000
 //                       76543210
 #define DATA_MASK_LO   0b01111111
 #define DATA_MASK_HI   0b10000000
 #define RAM_BANKMASK   0b00000011  // Supports banks 0-3 in upper nibble, here it's been downshifted by 4 bits
+#define ROM_BANKMASK   0b00001111  // Supports banks 0-15 in lower nybble
 #define RAM_BANK_SHIFT 4         // RAM bank bits are in the upper nibble
 
 #define RAM_BANK_COUNT         4
 #define RAM_BANK_SIZE          0x2000
+#define ROM_BANK_SIZE          0x8000  // 32K bank size, NOT split into 16K
 
 #define MD0_BANK_REGISTER_ADDR 0x1000 
 
@@ -99,10 +101,25 @@ unsigned char * const p_rambank_offsets[] = {
     RAM_BANK(3),  // 32k
 };
 
-void __not_in_flash_func( handleROM_MD0_cart_SRAM() ) {
-    // Initial bank, point it to BANK 0
-    uint8_t * rambank = RAM_BANK(0); // rom;
-    uint8_t LED_state = 1;
+
+#define ROM_BANK(N) (rom + (N * ROM_BANK_SIZE))
+
+const unsigned char * p_rombank_offsets[] = {
+    ROM_BANK(0),  ROM_BANK(1),  // 64K
+    ROM_BANK(2),  ROM_BANK(3),  // 128K
+    ROM_BANK(4),  ROM_BANK(5),  
+    ROM_BANK(6),  ROM_BANK(7),  // 256K
+    ROM_BANK(8),  ROM_BANK(9),
+    ROM_BANK(10), ROM_BANK(11),
+    ROM_BANK(12), ROM_BANK(13),
+    ROM_BANK(14), ROM_BANK(15), // 512K
+};
+
+
+void __not_in_flash_func( handleROM_MD0_with_SRAM() ) {
+    // Initial bank, point it to BANK 0 for both
+    uint8_t * rambank = RAM_BANK(0);       // SRAM
+    const uint8_t * rombank = ROM_BANK(0); // ROM
 
     // Start endless loop
     while( 1 ) {
@@ -114,32 +131,33 @@ void __not_in_flash_func( handleROM_MD0_cart_SRAM() ) {
         // All ROM Area 0x0000 - 0x7FFF addresses will have A15 line low
         if (!a15) {
 
-// NONE OF THIS CODE EVER SEEMS TO EXECUTE, Maybe when system ROM is active the entire ROM region is masked off from the ROM cart slot region
-
             // ROM Memory region
             uint32_t addr = bus_data & ADDRMASK;
 
-                                // Based on LED this appears to not get called
-                                // LED_state++; //  ^= 0x01;
-                                // gpio_put( LED_INT, LED_state & 0x01 );
-
             if (wr) {
-                // Data input.
+                // Data input
                 gpio_set_dir_in_masked( DATAMASK );
 
                 // Handle MD0 style bank switch register write at address 0x1000
-                // RAM bank is in upper 4 bits of data byte (but seems limited to values 0-3)
                 if (addr == MD0_BANK_REGISTER_ADDR) {
                     // Remap rambank to requested slice of cart SRAM buffer
+                    // RAM bank is in upper 4 bits of data byte (but seems limited to values 0-3)
                     rambank = p_rambank_offsets[(bus_data >> (DATAOFFSETLOW + RAM_BANK_SHIFT)) & RAM_BANKMASK];
-
-                                // Based on LED this appears to not get called
-                                // LED_state++; //  ^= 0x01;
-                                // gpio_put( LED_INT, LED_state & 0x01 );
+                    // ROM bank is in lower 4 bits of data byte, value range 0-15
+                    rombank = p_rombank_offsets[(bus_data >> (DATAOFFSETLOW )) & ROM_BANKMASK];
                 }
             } else {
-                // Data input, no ROM data on cart sram
-                gpio_set_dir_in_masked( DATAMASK );
+              // Data output
+              gpio_set_dir_out_masked( DATAMASK );
+              
+              // Get data byte based on whether it's in upper or lower 16K rom bank region.
+              uint8_t rombyte = rom[addr];
+              
+              uint32_t gpiobyte  = ( rombyte & DATA_MASK_LO ) << DATAOFFSETLOW;
+                       gpiobyte |= ( ( rombyte & DATA_MASK_HI ) >> 7 ) << DATAOFFSETHIGH;
+              
+              // And put it out
+              gpio_put_all( gpiobyte );                
             }
         }
         else {
@@ -162,13 +180,10 @@ void __not_in_flash_func( handleROM_MD0_cart_SRAM() ) {
                     // Write the data to the RAM buffer
                     rambank[cart_sram_relative_addr] = gpiobyte_in;
 
-                            // Based on LED this does appear to get accessed
-                                // LED_state ^= 0x01;
-                                // gpio_put( LED_INT, LED_state );
                 }
                 else {
                     // Data output
-//                    gpio_set_dir_out_masked( DATAMASK );
+                    gpio_set_dir_out_masked( DATAMASK );
 
                     uint8_t rambyte = rambank[cart_sram_relative_addr];
 
@@ -177,7 +192,7 @@ void __not_in_flash_func( handleROM_MD0_cart_SRAM() ) {
                     if (rambyte & DATA_MASK_HI) gpiobyte_out |= BUS_DATA_BIT_7;
 
                     // Put data byte out on bus
-//                    gpio_put_all( gpiobyte_out );  // This causes the crash, presumably related to gpio_set_dir_out_masked() above
+                    gpio_put_all( gpiobyte_out );  // This causes the crash, presumably related to gpio_set_dir_out_masked() above
                 } 
             }
             else {
@@ -198,11 +213,15 @@ void main() {
   // Turn on LED.
   gpio_put( LED_INT, 1 );
   
-
+  // Reset.
+  gpio_put( RST, 1 );
+  sleep_ms( RSTMS );
+  gpio_put( RST, 0 );
+  
   // Set RST pin back to INPUT, so we can use GPIO_PUT as non-masked.
   gpio_set_dir( RST, GPIO_IN );
   gpio_pull_down( RST );
   
-  handleROM_MD0_cart_SRAM();
+  handleROM_MD0_with_SRAM();
 
 }
